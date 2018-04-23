8c98c19a9acf89be4e89b8fc79b3c369a5952813
Admin <Admin@DESKTOP-88TTERB.mshome.net>
2018-04-23 16:18:12 +0300

    Tikhonov Andrey

---
+++ src/erpv2/App.java
@@ -0,0 +1,12 @@
+package erpv2;
+
+public class App {
+    private int id;
+    private String name;
+    private float price;
+    private String addDate;
+    private byte[] picture;
+    
+    
+
+}
